d565275012845704d9b3c4e800dbfb3a78f56972
Gust AF <ggaaaff@gmail.com>
2014-03-04 11:56:30 -0500

    Chapter 03 exercice 3-6 partial progress. Use of a vector to reduce the complexity of the code.

--- cap03/cap03-ex3_6-02-05-test-vector_random_letters_unique_restrict-loop.cpp
+++ cap03/cap03-ex3_6-02-05-test-vector_random_letters_unique_restrict-loop.cpp
@@ -1,4 +1,4 @@
-//2014.03.03 Gustaf - CTG.
+//2014.03.03 - 2014.03.04 Gustaf - CTG.
 
 
 
@@ -9,7 +9,7 @@ But they can't be repeated.
 
   Remember that you can’t substitute a letter for itself. So the first element can’t be A.
 
-Use a vector to see if the complexity of handling arrays is decreased.
+Use a vector and check if the complexity of handling arrays is decreased.
 
 
 ------Information
@@ -19,12 +19,52 @@ vectors are very efficient accessing its elements (just like arrays) and relativ
 For operations that involve inserting or removing elements at positions other than the end, they perform worse than the others.
 
 
+---WARNING
+The letter Z can appear at the end, a prove:
+
+            GETTING THE LETTER (countLetter - range): 24 - 3
+            Possible letter (char - random number - lettersAvailableCount) C - 1 - 2
+            Letter (char - int - index) C - 67 - 2
+            availableLettersVector: Z C T
+             To erase()
+            availableLettersVector: Z T
+
+            Got the letter: C
+
+
+            GETTING THE LETTER (countLetter - range): 25 - 2
+            Possible letter (char - random number - lettersAvailableCount) T - 1 - 1
+            Letter (char - int - index) T - 84 - 19
+            availableLettersVector: Z T
+             To erase()
+            availableLettersVector: Z
+
+            Got the letter: T
+
+
+            GETTING THE LETTER (countLetter - range): 26 - 1
+            Possible letter (char - random number - lettersAvailableCount) Z - 0 - 0
+            Letter (char - int - index) Z - 90 - 25
+            Letter verification - We CAN NOT try another letter.
+
+            availableLettersVector: Z
+             To erase()
+            availableLettersVector:
+
+            Got the letter: Z
+
+
+            The final array.
+            S E N J K V L W D G B U X Y A M P F R I O H Q C T Z
+
+
+
 
 
 === PLAN ===
 
-TO DO- Use a vector for the available letters.
-  FAIL - Use the function vector.erase()
+OK - Use a vector for the available letters.
+  OK - Use the function vector.erase()
          http://www.cplusplus.com/reference/vector/vector/erase/
 
 */
@@ -48,7 +88,7 @@ int main()
                                      , 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
                                    };
 
-  //2014.03.01 NOTE: Moving the letter 'Z' eliminate the chance that it appear at the end of the final array.
+  //2014.03.04 NOTE: Moving the letter 'Z' REDUCE the chance that it appear at the end of the final array.
   char availableLetters[ARRAY_SIZE] = { 'A', 'Z', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
                                         , 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'
                                       };
@@ -67,7 +107,8 @@ int main()
   //Initialize vector.
   for (int i = 0; i < (ARRAY_SIZE); ++i)
   {
-    availableLettersVector[i] = availableLetters[i];
+    // NOTE 2014.03.04 ALLWAYS use push_back() to add an element.
+    availableLettersVector.push_back(availableLetters[i]);
   }
 
 
@@ -91,7 +132,7 @@ int main()
     highest = lettersAvailableCount;
     range = (highest - lowest) + 1; // This range decreases while the loop progress.
 
-    cout << "GETTING THE LETTER NUMBER: " << countLetter << " - " << range << endl; //DEBUG
+    cout << "GETTING THE LETTER (countLetter - range): " << countLetter << " - " << range << endl; //DEBUG
 
 
     // Testing possible letters
@@ -99,7 +140,7 @@ int main()
     while ( possibleLetter == '@'  )
     {
       randomNumber = lowest + rand() % range;
-      possibleLetter = availableLetters[randomNumber];
+      possibleLetter = availableLettersVector[randomNumber];
 
       cout << "Possible letter (char - random number - lettersAvailableCount) " << possibleLetter << " - " << randomNumber << " - " << lettersAvailableCount << endl; // DEBUG
 
@@ -141,72 +182,37 @@ int main()
     }
 
 
-
-    // We got the new letter
-    substitutionLetters[ countLetter - 1 ] = possibleLetter;
-    
-    //NOTE 2014.03.03 there is aproblen with the use of namespaces. I can't solve it with my actual knowledge of C++.
-    availableLettersVector.erase(randomNumber); //the letter is no longer available.
-
-
-    cout << endl << "Got the letter: " << possibleLetter  << endl; //DEBUG
-
-
-
-
-//-------------------
-/*
-    // We got the new letter
-    substitutionLetters[ countLetter - 1 ] = possibleLetter;
-    availableLetters[randomNumber] = '#'; //the letter is no longer available.
-
-    cout << endl << "Got the letter: " << possibleLetter  << endl; //DEBUG
-
-
-    //Move the available letters to a temporary array
-    char auxArray[lettersAvailableCount]; // Nice trick in C++, the dinamic creation.
-    int j = 0;
-    for (int i = 0; i < ARRAY_SIZE; ++i)
-    {
-      if ( availableLetters[i] != '#')
-      {
-        auxArray[j] = availableLetters[i];
-        j++;
-      }
-    }
-
     //--- Output for DEBUG
-    for (int i = 0; i < (ARRAY_SIZE); ++i) //DEBUG
+    cout << "availableLettersVector: ";
+    for (int i = 0; i < availableLettersVector.size(); ++i)
     {
-      cout << availableLetters[i] << " ";
+      cout << availableLettersVector[i] << " ";
     }
-    cout << endl;
-
-    for (int i = 0; i < (lettersAvailableCount); ++i) //DEBUG
-    {
-      cout << auxArray[i] << " ";
-    }
-    cout << endl;
     cout << endl;
     //---
 
 
-    //Update the array with the available letters
-    for (int i = 0; i < (ARRAY_SIZE); ++i)
-    {
-      availableLetters[i] = auxArray[i];
+    // We got the new letter
+    substitutionLetters[ countLetter - 1 ] = possibleLetter;
 
-      if (i >= (lettersAvailableCount))
-      {
-        availableLetters[i] = '#';
-      }
-    }*/
+    // NOTE 2014.03.03 There is a problem (maybe) with the use of namespaces.
+    //      error: no matching function for call to ‘std::vector<char>::erase(int&)’
+    //      I can't solve this with my actual knowledge of C++.
+    // SOLVED 2014.03.04
+    //      This was the line with the problem: availableLettersVector.erase(randomNumber);
+    //      I was missing the call to the ITERATOR: vector.begin().
+    //      Remember that vector works like arrays but (I guess???) are pointers̉.
+    //      source: http://www.daniweb.com/software-development/cpp/threads/214059/error-erasing-element-from-a-vector
 
-//--------------
+    cout << " To erase() " << endl; //DEBUG
+    availableLettersVector.erase(availableLettersVector.begin() + randomNumber); //the letter is no longer available.
 
 
 
     //--- Output for DEBUG
+    cout << endl << "Got the letter: " << possibleLetter  << endl; //DEBUG
+    cout << endl;
+
     cout << "substitutionLetters: ";
     for (int i = 0; i < countLetter; ++i)
     {
@@ -215,10 +221,12 @@ int main()
     cout << endl;
 
     cout << "availableLettersVector: ";
-    for (int i = 0; i < (ARRAY_SIZE); ++i)
+    for (int i = 0; i < availableLettersVector.size(); ++i)
     {
       cout << availableLettersVector[i] << " ";
     }
+    cout << endl;
+
     cout << endl;
     cout << endl;
     //---
