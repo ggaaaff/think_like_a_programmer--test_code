47baea447864cc6262890a72cc87cdde2dcdc26e
Gust AF <ggaaaff@gmail.com>
2014-03-10 09:53:58 -0500

    Chapter 03 exercice 3-7 completed.

---
+++ cap03/cap03-ex3_7-01-statistic-mode.cpp
@@ -0,0 +1,34 @@
+//2014.03.10 Gustaf - CTG.
+
+
+/* EXERCISE 3-7:
+
+Write a program that is given an array of integers and determines the mode,
+which is the number that appears most frequently in the array.
+
+
+I think is the same to this: cap03-02-01-statistic_mode.cpp
+
+
+=== PLAN ===
+
+- 
+- 
+
+*/
+
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+  cout << "Hello world - Linux Mint." << endl;
+
+  cout << endl;
+  return 0;
+}
