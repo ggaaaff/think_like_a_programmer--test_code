509133424adc973aaab9839401695e2941d8dda5
Gust AF <ggaaaff@gmail.com>
2014-03-10 11:16:43 -0500

    Chapter 03 exercice 3-8 partial progress.

---
+++ cap03/cap03-ex3_8-01-statistic-quartiles.cpp
@@ -0,0 +1,59 @@
+//2014.03.10 Gustaf - CTG.
+
+
+/* EXERCISE 3-8:
+Write a program that processes an array of student objects and determines
+the grade quartiles — that is, the grade one would need to score as well as or
+ better than 25% of the students, 50% of the students, and 75% of the students.
+
+
+
+==== INFORMATION ===
+http://en.wikipedia.org/wiki/Quartile
+
+In descriptive statistics, the quartiles of a ranked set of data values are the three points 
+that divide the data set into four equal groups, each group comprising a quarter of the data.
+
+  The first quartile (Q1) is defined as the middle number between the smallest number and the median of the data set. 
+  The second quartile (Q2) is the median of the data. 
+  The third quartile (Q3) is the middle value between the median and the highest value of the data set.
+
+
+  Two examples:
+  http://en.wikipedia.org/wiki/Quantile#Examples
+
+
+
+------------------
+A crystal clear example:
+http://www.vias.org/tmdatanaleng/cc_quartile.html
+
+A online Calculator:
+http://www.hackmath.net/en/calculator/quartile-q1-q2-q3-calculation
+
+
+
+
+=== PLAN ===
+
+OK - Get information.
+- Select a method (one out of three) to do the calculation.
+- Calculate the quartiles.
+
+*/
+
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+  cout << "Process of students quartiles." << endl;
+
+  cout << endl;
+  return 0;
+}
