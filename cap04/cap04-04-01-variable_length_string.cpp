11e190e2eba19269271660709eeed7f6065e1df1
Gust AF <ggaaaff@gmail.com>
2014-03-18 23:55:47 -0500

    Chapter 04 examples. Initial work on problem about string manipulation.

---
+++ cap04/cap04-04-01-variable_length_string.cpp
@@ -0,0 +1,47 @@
+//2014.03.18 Gustaf - CTG.
+
+
+/* P R O B L E M : V A R I A B L E - L E N G T H  S T R I N G  M A N I P U L A T I O N
+
+Write heap-based implementations for three required string functions:
+
+  - append
+  This function takes a string and a character and appends the character
+  to the end of the string.
+
+  - concatenate 
+  This function takes two strings and appends the characters of the
+  second string onto the first.
+
+  - characterAt 
+  This function takes a string and a number and returns the character
+  at that position in the string (with the first character in the string numbered zero).
+
+
+  Write the code with the assumption that characterAt will be called frequently,
+  while the other two functions will be called relatively seldom. The relative efficiency of
+  the operations should reflect the calling frequency.
+
+
+
+=== PLAN ===
+
+- 
+- 
+
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+  cout << "Hello world - Linux Mint." << endl;
+
+  cout << endl;
+  return 0;
+}
